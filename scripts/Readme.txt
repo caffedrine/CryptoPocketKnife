068b55ac2277235b98ffada7517ee35138d5711f
caffedrine <bacchusstyle@yahoo.com>
2021-07-12 11:26:36 +0300

    - Added icon - Created a folder with deployed app

---
+++ scripts/Readme.txt
@@ -0,0 +1,3 @@
+# Windows deploy
+
+win-deploy-qt.bat "C:/Qt/5.12.11/mingw73_64" "../build/Release/CryptoPocketKnife.exe" "../build/Deployed/"
--- ui/mainwindow.cpp
+++ ui/mainwindow.cpp
@@ -6,7 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    this->setWindowIcon(QIcon(":/res/img/logo.png"));
+
 }
 
 MainWindow::~MainWindow()
