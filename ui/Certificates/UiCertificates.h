feefd3ecbbe1640532a57a0011e9bfaf9587cf20
caffedrine <bacchusstyle@yahoo.com>
2022-12-15 12:50:00 +0200

    Add separate handler for x509 certificate Build release with new Qt version

--- ui/Certificates/UiCertificates.h
+++ ui/Certificates/UiCertificates.h
@@ -6,7 +6,12 @@
 #include <QSslKey>
 #include <QSslCertificate>
 #include <QSslCertificateExtension>
-
+#include "X509Certificate.h"
+//
+// USefully to autodetect multiple certificates in the same string
+// https://letsencrypt.org/docs/a-warm-welcome-to-asn1-and-der/
+// So now we know that the first two bytes of the DER encoding of a certificate are 0x30 0x82. PEM encoding uses base64, which encodes 3 bytes of binary input into 4 ASCII characters of output. Or, to put it differently: base64 turns 24 bits of binary input into 4 ASCII characters of output, with 6 bits of the input assigned to each character.
+//
 
 QT_BEGIN_NAMESPACE
 namespace Ui
@@ -46,7 +51,6 @@ private:
     void ParseCert();
     void ParseCSR();
     QString ParseCertCsrInputStringToB64(QString);
-    QString GetCertHumanReadableFormat(const QByteArray &certificate);
 
     void Status_EndWithError(QString err_msg);
     void Status_EndWithSuccess(QString err_msg);
