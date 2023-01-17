7ac282464dfd62a381ff1686a6514299c3cb2805
caffedrine <bacchusstyle@yahoo.com>
2023-01-17 11:58:56 +0200

    Finished project refractoring

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,15 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
 
-//    // Show debug info when app is executed from console
-//    #ifdef _WIN32
-//        /// https://stackoverflow.com/questions/3360548/console-output-in-a-qt-gui-app
-//        if (AttachConsole(ATTACH_PARENT_PROCESS))
-//        {
-//            freopen("CONOUT$", "w", stdout);
-//            freopen("CONOUT$", "w", stderr);
-//        }
-//    #endif
+    // Show debug info when app is executed from console
+    #ifdef _WIN32
+        /// https://stackoverflow.com/questions/3360548/console-output-in-a-qt-gui-app
+        if (AttachConsole(ATTACH_PARENT_PROCESS))
+        {
+            freopen("CONOUT$", "w", stdout);
+            freopen("CONOUT$", "w", stderr);
+        }
+    #endif
 
     base::setup_logger(false);
     base::logger->info("App started...");
